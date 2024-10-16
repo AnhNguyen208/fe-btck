24789c952e32396c3f773c6b06a966cf302b11a4
LA <nguyenletuananh20082001@gmail.com>
2024-10-16 16:22:59 +0700

    update error message

--- src/app/model/error-messages.ts
+++ src/app/model/error-messages.ts
@@ -15,12 +15,15 @@ export enum ErrorMessages {
 
     // 1.4 Validate parameter [employeeBirthDate]
     ER001_EMPLOYEE_BIRTHDATE = "「生年月日」を入力してください。",
-    ER005_EMPLOYEE_BIRTHDATE = "「生年月日」をyyyy/MM/dd形式で入力してください。",
+    // ER005_EMPLOYEE_BIRTHDATE = "「生年月日」をyyyy/MM/dd形式で入力してください。",
     ER011_EMPLOYEE_BIRTHDATE = "「生年月日」は無効になっています。",
 
     // 1.5 Validate parameter [employeeEmail]
     ER001_EMPLOYEE_EMAIL = "「メールアドレス」を入力してください。",
     ER006_EMPLOYEE_EMAIL = "125桁以内の「メールアドレス」を入力してください。",
+    ER005_EMPLOYEE_EMAIL = "「メールアドレス」をhalfsize形式で入力してください",
+    ER005_EMPLOYEE_EMAIL_FORMAT = "「メールアドレス」をemail(@.)形式で入力してください",
+    // format email ER005
 
     // 1.6 Validate parameter [employeeTelephone]
     ER001_EMPLOYEE_TELEPHONE = "「電話番号」を入力してください。",
@@ -33,7 +36,7 @@ export enum ErrorMessages {
 
     // 1.8 Validate parameter [departmentId]
     ER002_DEPARTMENT_ID = "「グループ」を入力してください。",
-    ER004_DEPARTMENT_ID = "「グループ」は存在していません。",
+    // ER004_DEPARTMENT_ID = "「グループ」は存在していません。",
     ER018_DEPARTMENT_ID = "「グループ」は半角で入力してください。",
 
     // 1.9 Validate parameter [certifications]
@@ -42,11 +45,15 @@ export enum ErrorMessages {
     ER001_CERTIFICATION_SCORE = "「点数」を入力してください。",
     ER001_CERTIFICATION_ID = "「資格」を入力してください。",
     ER004_CERTIFICATION_ID = "「資格」は存在していません。",
-    ER005_CERTIFICATION_START_DATE = "「資格交付日」をyyyy/MM/dd形式で入力してください。",
-    ER005_CERTIFICATION_END_DATE = "「失効日」をyyyy/MM/dd形式で入力してください。",
+    // ER005_CERTIFICATION_START_DATE = "「資格交付日」をyyyy/MM/dd形式で入力してください。",
+    // ER005_CERTIFICATION_END_DATE = "「失効日」をyyyy/MM/dd形式で入力してください。",
     ER012_CERTIFICATION_START_DATE_END_DATE = "「失効日」は「資格交付日」より未来の日で入力してください。",
     ER018_CERTIFICATION_SCORE = "「点数」は半角で入力してください。",
     ER018_CERTIFICATION_ID = "「資格」は半角で入力してください。",
 
-    ER023 = "システムエラーが発生しました。",
+    // ER023 = "システムエラーが発生しました。",
+
+    // Validate confirm password
+    ER001_EMPLOYEE_LOGIN_CONFIRM_PASSWORD = "「パスワード（確認）」を入力してください。",
+    ER017_EMPLOYEE_LOGIN_CONFIRM_PASSWORD = "「パスワード（確認）」が不正です。",
 }
